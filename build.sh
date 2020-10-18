efd58369ea47482cc11b353be73a4f3faef6cb8f
Michael <michael.dogali@gmail.com>
2020-10-18 16:53:18 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /tmp
+curl -sL -o fail2ban-latest.txt https://raw.githubusercontent.com/redoracle/docker_fail2ban/main/release-versions/fail2ban-latest.txt \
+F2BVERSION=$(cat ./fail2ban-latest.txt) 
+export FAIL2BAN_VERSION="$F2BVERSION" 
+export TZ="UTC" 
+curl -SsOL https://github.com/fail2ban/fail2ban/archive/${FAIL2BAN_VERSION}.zip 
+unzip ${FAIL2BAN_VERSION}.zip 
+cd fail2ban-${FAIL2BAN_VERSION} 
+2to3 -w --no-diffs bin/* fail2ban 
+python3 setup.py install 
